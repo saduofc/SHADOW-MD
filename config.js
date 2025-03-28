e77fc725eb0ccaa1d18c7af69681896bd76bc72e
Nethuni2007 <suranjalokuge@gmail.com>
2025-03-28 23:18:16 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ function convertToBool(text, fault = 'true') {
 //--------------------------Malvin King--------------------------------------
 module.exports = {
     SESSION_ID: process.env.SESSION_ID || "EIRAyLIL#muIIuQ93eV5sbYK8JKA3EdCnShlQyThPdJxS8tVAM5c",
-    CAPTION: process.env.CAPTION || "*©  ʙʏ ʟᴏᴋᴜ ɴɪᴍᴀʜ*",
+    CAPTION: process.env.CAPTION || "*©  BY CYEBER SHADOW FAMLIY *",
     ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "true",
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
     READ_MESSAGE: process.env.READ_MESSAGE || "false", // Added auto-read configuration
@@ -18,19 +18,19 @@ module.exports = {
     MODE: process.env.MODE || "public",
     AUTO_VOICE: process.env.AUTO_VOICE || "true",
     AUTO_STICKER: process.env.AUTO_STICKER || "false",
-    AUTO_REPLY: process.env.AUTO_REPLY || "true",
-    ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/zqoycp.jpg",
-    ALIVE_MSG: process.env.ALIVE_MSG || "𝗛𝗜 𝗔𝗠 𝗔𝗟𝗜𝗩𝗘 𝗡𝗢𝗪 𝗔𝗠 𝗡𝗜𝗠𝗔-𝗠𝗗 𝗪𝗛𝗔𝗧𝗦𝗔𝗣𝗣 𝗕𝗢𝗧🚀",
+    AUTO_REPLY: process.env.AUTO_REPLY || "false",
+    ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/eigd9d.jpg",
+    ALIVE_MSG: process.env.ALIVE_MSG || "HI AM ALIVE NOW AM SHADOW-MD WHATSAPP BOT🚀",
     ANTI_LINK: process.env.ANTI_LINK || "false",
     ANTI_BAD: process.env.ANTI_BAD || "true",
     PREFIX: process.env.PREFIX || ".",
     FAKE_RECORDING: process.env.FAKE_RECORDING || "true",
-    AUTO_REACT: process.env.AUTO_REACT || "true",
-    HEART_REACT: process.env.HEART_REACT || "true",
-    OWNER_REACT: process.env.OWNER_REACT || "true",
-    BOT_NAME: process.env.BOT_NAME || "『 NIMA-MD 』",
+    AUTO_REACT: process.env.AUTO_REACT || "false",
+    HEART_REACT: process.env.HEART_REACT || "false",
+    OWNER_REACT: process.env.OWNER_REACT || "false",
+    BOT_NAME: process.env.BOT_NAME || "『 SHADOW-MD 』",
     AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
-    AUTO_STATUS__MSG: process.env.AUTO_STATUS__MSG || "`Status Seen By NIMA-MD Bot 🦄 `",
+    AUTO_STATUS__MSG: process.env.AUTO_STATUS__MSG || "`Status Seen By SHADOW-MD Bot 🦄 `",
     OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39", // omdbapi.com
 };
 //--------------MADE BY MALVIN KING 2025---------------->-
